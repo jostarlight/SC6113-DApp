57fbf07bb74934a729399263bda80a3ec7e5f27d
CleaZhang <57275818+CleaZhang@users.noreply.github.com>
2024-08-27 22:26:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask,render_template,request
 
 app=Flask(__name__)
 
-@app.route('/',methods=["GET","POST"])
+@app.route('/',methods=["get","post"])
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
 if __name__=='__main__':
-  app.run()
+    app.run()
